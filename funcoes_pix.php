e0948fa6dd1f83262835a4314d0f09a6fc2028a8
Renato Monteiro Batista <r3n4t0@gmail.com>
2021-07-17 20:47:14 -0300

    Tratamento caracteres especiais. Resolve #5. Resolve #6.

--- funcoes_pix.php
+++ funcoes_pix.php
@@ -24,6 +24,7 @@ function montaPix($px){
    foreach ($px as $k => $v) {
      if (!is_array($v)) {
         if ($k == 54) { $v=number_format($v,2,'.',''); } // Formata o campo valor com 2 digitos.
+        else { $v=remove_char_especiais($v); }
         $ret.=c2($k).cpm($v).$v;
      }
      else {
@@ -32,7 +33,44 @@ function montaPix($px){
      }
    }
    return $ret;
- }
+}
+
+function remove_char_especiais($txt){
+   /*
+   # Esta função retorna somente os caracteres alfanuméricos (a-z,A-Z,0-9) de uma string.
+   # Caracteres acentuados são convertidos pelos equivalentes sem acentos.
+   # Emojis são removidos.
+   #
+   # Autor: Eng. Renato Monteiro Batista
+   */
+   return preg_replace('/\W/','',remove_acentos($txt));
+}
+
+function remove_acentos($texto){
+   /*
+   # Esta função retorna uma string substituindo os caracteres especiais de acentuação
+   # pelos respectivos caracteres não acentuados em português-br.
+   #
+   # Autor: Eng. Renato Monteiro Batista
+   */
+   $search = explode(",","à,á,â,ä,æ,ã,å,ā,ç,ć,č,è,é,ê,ë,ē,ė,ę,î,ï,í,ī,į,ì,ł,ñ,ń,ô,ö,ò,ó,œ,ø,ō,õ,ß,ś,š,û,ü,ù,ú,ū,ÿ,ž,ź,ż,À,Á,Â,Ä,Æ,Ã,Å,Ā,Ç,Ć,Č,È,É,Ê,Ë,Ē,Ė,Ę,Î,Ï,Í,Ī,Į,Ì,Ł,Ñ,Ń,Ô,Ö,Ò,Ó,Œ,Ø,Ō,Õ,Ś,Š,Û,Ü,Ù,Ú,Ū,Ÿ,Ž,Ź,Ż");
+   $replace =explode(",","a,a,a,a,a,a,a,a,c,c,c,e,e,e,e,e,e,e,i,i,i,i,i,i,l,n,n,o,o,o,o,o,o,o,o,s,s,s,u,u,u,u,u,y,z,z,z,A,A,A,A,A,A,A,A,C,C,C,E,E,E,E,E,E,E,I,I,I,I,I,I,L,N,N,O,O,O,O,O,O,O,O,S,S,U,U,U,U,U,Y,Z,Z,Z");
+   return remove_emoji(str_replace($search, $replace, $texto));
+}
+
+function remove_emoji($string){
+   /*
+   # Esta função retorna o conteúdo de uma string removendo oas caracteres especiais
+   # usados para representação de emojis.
+   #
+   */
+   return preg_replace('%(?:
+   \xF0[\x90-\xBF][\x80-\xBF]{2}      # planes 1-3
+ | [\xF1-\xF3][\x80-\xBF]{3}          # planes 4-15
+ | \xF4[\x80-\x8F][\x80-\xBF]{2}      # plane 16
+)%xs', '  ', $string);      
+}
+
 
 function cpm($tx){
     /*
